82ab695ce9db0eb8170e25c41cccb8f5c836a48f
Leandro Ritter <leandroritter@MacBook-Pro-de-Leandro.local>
2021-04-27 12:45:55 -0300

    Refactored module, add data patch and i18n

---
+++ app/code/Innovatis/BundleLimiter/Setup/Patch/Data/CreateBasketActiveAttribute.php
@@ -0,0 +1,100 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Innovatis\BundleLimiter\Setup\Patch\Data;
+
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Catalog\Setup\CategorySetupFactory;
+
+/**
+ * Patch is mechanism, that allows to do atomic upgrade data changes
+ */
+class CreateBasketActiveAttribute implements DataPatchInterface
+{
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+    private EavSetupFactory $eavSetupFactory;
+    private CategorySetupFactory $categorySetupFactory;
+
+
+    public function __construct(
+        ModuleDataSetupInterface $resourceConnection,
+        EavSetupFactory $eavSetupFactory,
+        CategorySetupFactory $categorySetupFactory
+    ) {
+        $this->moduleDataSetup = $resourceConnection;
+        $this->eavSetupFactory = $eavSetupFactory;
+        $this->categorySetupFactory = $categorySetupFactory;
+    }
+
+    /**
+     * Do Upgrade
+     *
+     * @return void
+     */
+    public function apply()
+    {
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $categorySetup = $this->categorySetupFactory->create(['setup' => $this->moduleDataSetup]);
+
+        $attributeCode = 'basket_active';
+
+        $eavSetup->addAttribute(
+            \Magento\Catalog\Model\Product::ENTITY,
+            $attributeCode,
+            [
+                'group' => 'General',
+                'type' => 'int',
+                'backend' => '',
+                'frontend' => '',
+                'label' => 'Basket Active',
+                'input' => 'boolean',
+                'class' => '',
+                'source' => \Magento\Eav\Model\Entity\Attribute\Source\Boolean::class,
+                'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_GLOBAL,
+                'visible' => true,
+                'required' => false,
+                'user_defined' => false,
+                'default' => '1',
+                'searchable' => false,
+                'filterable' => false,
+                'comparable' => false,
+                'visible_on_front' => false,
+                'used_in_product_listing' => false,
+                'unique' => false,
+                'apply_to' => 'bundle'
+            ]
+        );
+
+        $attributeSetId = $categorySetup->getDefaultAttributeSetId(\Magento\Catalog\Model\Product::ENTITY);
+
+        $categorySetup->addAttributeToGroup(
+            \Magento\Catalog\Model\Product::ENTITY,
+            $attributeSetId,
+            'General', // attribute group
+            $attributeCode, // attribute code
+            20 // sort order
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+}
