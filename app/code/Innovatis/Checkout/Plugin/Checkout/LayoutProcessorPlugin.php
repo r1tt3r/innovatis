e1e4dc862b99170b71e9f64d4fa5091cfd453f9a
Leandro Ritter <leandroritter@MacBook-Pro-de-Leandro.local>
2021-04-28 14:23:50 -0300

    Add autocomplete for address fields, re-order inputs and some CSS ajusts

---
+++ app/code/Innovatis/Checkout/Plugin/Checkout/LayoutProcessorPlugin.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Innovatis\Checkout\Plugin\Checkout;
+
+class LayoutProcessorPlugin
+{
+    /**
+     * @param \Magento\Checkout\Block\Checkout\LayoutProcessor $subject
+     * @param array $jsLayout
+     * @return array
+     */
+    public function afterProcess(
+        \Magento\Checkout\Block\Checkout\LayoutProcessor $subject,
+        array  $jsLayout
+    ) {
+        $jsLayout['components']['checkout']['children']['steps']['children']['shipping-step']['children']
+        ['shippingAddress']['children']['shipping-address-fieldset']['children']['street'] = [
+            'component' => 'Magento_Ui/js/form/components/group',
+            'label' => __('Street Address'),
+            'required' => true,
+            'dataScope' => 'shippingAddress.street',
+            'provider' => 'checkoutProvider',
+            'sortOrder' => 60,
+            'type' => 'group',
+            'additionalClasses' => 'street',
+            'children' => [
+                [
+                    'label' => __('Streets'),
+                    'component' => 'Magento_Ui/js/form/element/abstract',
+                    'config' => [
+                        'customScope' => 'shippingAddress',
+                        'template' => 'ui/form/field',
+                        'elementTmpl' => 'ui/form/element/input'
+                    ],
+                    'dataScope' => '0',
+                    'provider' => 'checkoutProvider',
+                    'validation' => ['required-entry' => true, "min_text_len‌​gth" => 1, "max_text_length" => 255],
+                ],
+                [
+                    'label' => __('Street Number'),
+                    'component' => 'Magento_Ui/js/form/element/abstract',
+                    'config' => [
+                        'customScope' => 'shippingAddress',
+                        'template' => 'ui/form/field',
+                        'elementTmpl' => 'ui/form/element/input'
+                    ],
+                    'dataScope' => '1',
+                    'provider' => 'checkoutProvider',
+                    'validation' => ['required-entry' => true, "min_text_len‌​gth" => 1, "max_text_length" => 255],
+                ],
+                [
+                    'label' => __('Address complement'),
+                    'component' => 'Magento_Ui/js/form/element/abstract',
+                    'config' => [
+                        'customScope' => 'shippingAddress',
+                        'template' => 'ui/form/field',
+                        'elementTmpl' => 'ui/form/element/input'
+                    ],
+                    'dataScope' => '2',
+                    'provider' => 'checkoutProvider',
+                    'validation' => ['required-entry' => false, "min_text_len‌​gth" => 1, "max_text_length" => 255],
+                ],
+                [
+                    'label' => __('Neighborhood'),
+                    'component' => 'Magento_Ui/js/form/element/abstract',
+                    'config' => [
+                        'customScope' => 'shippingAddress',
+                        'template' => 'ui/form/field',
+                        'elementTmpl' => 'ui/form/element/input'
+                    ],
+                    'dataScope' => '3',
+                    'provider' => 'checkoutProvider',
+                    'validation' => ['required-entry' => false, "min_text_len‌​gth" => 1, "max_text_length" => 255],
+                ],
+            ]
+        ];
+        return $jsLayout;
+    }
+}
